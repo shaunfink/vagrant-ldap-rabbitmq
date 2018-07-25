3b122cfe059d541a052d1339d69695f4ba85214c
Shaun Fink <shaun.fink@rbs.co.uk>
2018-07-25 12:49:01 +0100

    Trying out some other configs

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -12,16 +12,6 @@ openldap::server::globalconf { 'ServerID':
   value  => { 'ServerID' => [ '1 ldap://rabbitmq.dev' ] }
 } ->
 
-# openldap::server::access { '0 on dc=rabbitmq,dc=dev':
-#   what     => 'attrs=userPassword,shadowLastChange',
-#   access   => [
-#     'by dn="cn=admin,dc=rabbitmq,dc=dev" write',
-#     'by anonymous auth',
-#     'by self write',
-#     'by * none',
-#   ],
-# } ->
-
 # Import users into LDAP
 exec { 'ldapadd':
   path    => '/usr/bin',
@@ -31,27 +21,46 @@ exec { 'ldapadd':
 
 # Provsion RabbitMQ
 class { 'rabbitmq':
+  delete_guest_user           => true,
   admin_enable                => true,
   management_ssl              => false,
   management_hostname         => 'rabbitmq.dev',
   ssl                         => true,
   port                        => 5672,
   ssl_port                    => 5671,
+  ssl_depth                   => 3,
   ssl_cacert                  => '/vagrant_data/certs/cacert.pem',
   ssl_cert                    => '/vagrant_data/certs/cert.pem',
   ssl_key                     => '/vagrant_data/certs/key.pem',
   ssl_verify                  => 'verify_peer',
   ssl_fail_if_no_peer_cert    => true,
-  #auth_backends               => ['ldap', 'internal'],
+  auth_backends               => ['rabbit_auth_backend_ldap', 'rabbit_auth_backend_internal'],
   ldap_auth                   => true,
-  ldap_server                 => 'ldap://localhost',
+  ldap_server                 => 'rabbitmq.dev',
   ldap_port                   => 389,
-  #ldap_user_dn_pattern        =>'cn=${username},ou=services,dc=rabbitmq,dc=dev',
-  ldap_user_dn_pattern        =>'${username}',
+  ldap_user_dn_pattern        => 'cn=${username},ou=services,dc=rabbitmq,dc=dev',
+  #ldap_other_bind             => "{'cn=rabbitbind,ou=services,dc=rabbitmq,dc=dev', 'rabbitbind'}",
+  ldap_other_bind             => "{'cn=admin,dc=rabbitmq,dc=dev', 'secret'}",
+  #ldap_other_bind             => "{'rabbitbind', 'rabbitbind'}",
   ldap_log                    => true,
-  #config_additional_variables => {
-  #  rabbit => '[{auth_mechanisms, [EXTERNAL, PLAIN]}]'
-  #}
+  config_variables            => {
+    ssl_cert_login_from       => 'common_name',
+    auth_mechanisms           => "['EXTERNAL', 'AMQPLAIN', 'PLAIN']",
+    log_levels                => "[{connection, debug}, {default, debug}, {channel, debug}, {queue, debug}, {mirroring, debug}, {federation, debug}, {upgrade, debug}]"
+  },
+  ldap_config_variables       => {
+    log                       => 'network_unsafe',
+    #dn_lookup_base            => "'ou=services,dc=rabbitmq,dc=dev'",
+    #dn_lookup_attribute       => "'cn'",
+    #group_lookup_base         => "'ou=groups,dc=rabbitmq,dc=dev'",
+    tag_queries               => "[{administrator, {constant, true}}, {management, {constant, true}}]",
+    vhost_access_query        => "{constant, true}",
+    #vhost_access_query        => "{in_group, 'cn=services,ou=groups,dc=rabbitmq,dc=dev'}",
+    resource_access_query     => "{constant, true}",
+    #resource_access_query     => "{in_group, 'cn=services,ou=groups,dc=rabbitmq,dc=dev'}",
+    topic_access_query        => "{constant, true}"
+    #topic_access_query        => "{in_group, 'cn=services,ou=groups,dc=rabbitmq,dc=dev'}",
+  }
 }
 
 # Enable SSL Auth plugin
@@ -70,23 +79,44 @@ rabbitmq_user { 'rabbitadmin':
  admin    => true,
  password => 'rabbitadminlocal',
 }
-rabbitmq_user { 'rabbitdevadmin':
+
+rabbitmq_user { 'rabbitbind':
  ensure   => 'present',
  admin    => true,
- password => 'rabbitdevadminlocal',
+ password => 'rabbitbindlocal',
 }
+
+rabbitmq_user { 'rabbitdev':
+ ensure   => 'present',
+ admin    => true,
+ password => 'rabbitdevlocal',
+}
+
 rabbitmq_user { 'rabbitdevcode':
  ensure   => 'present',
- admin    => false,
+ admin    => true,
  password => 'rabbitdevcodelocal',
 }
 
-# Set permissions for users
-rabbitmq_user_permissions { 'rabbitdevadmin@devvhost':
+# Assign permissions to users
+rabbitmq_user_permissions { 'rabbitadmin@/':
   configure_permission => '.*',
   read_permission      => '.*',
   write_permission     => '.*',
 }
+
+# rabbitmq_user_permissions { 'rabbitadmin@devvhost':
+#   configure_permission => '.*',
+#   read_permission      => '.*',
+#   write_permission     => '.*',
+# }
+
+rabbitmq_user_permissions { 'rabbitdev@devvhost':
+  configure_permission => '.*',
+  read_permission      => '.*',
+  write_permission     => '.*',
+}
+
 rabbitmq_user_permissions { 'rabbitdevcode@devvhost':
   configure_permission => '.*',
   read_permission      => '.*',
