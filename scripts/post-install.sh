d3ec84c001ca7fe78061296666f7193610428c76
Shaun Fink <shaun.fink@rbs.co.uk>
2018-07-25 15:27:19 +0100

    Updated post install for better logging

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,2 +1,8 @@
 # Enable the rabbitmq_auth_mechanism_ssl plugin, as the puppet module doesn't seem to do it.
-sudo rabbitmq-plugins enable rabbitmq_auth_mechanism_ssl
+# sudo rabbitmq-plugins enable rabbitmq_auth_mechanism_ssl
+
+# Update ldap log to network_unsafe for super detailed info
+sudo sed -i 's/log, true/log, network_unsafe/g' /etc/rabbitmq/rabbitmq.config
+
+# Restart rabbitmq after this change
+sudo systemctl restart rabbitmq-server
