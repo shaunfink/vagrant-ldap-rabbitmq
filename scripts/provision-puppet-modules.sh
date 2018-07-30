a3197cae7addd394301f3870262036bf25053875
Shaun Fink <shaun.fink@rbs.co.uk>
2018-07-30 09:05:13 +0100

    fixed puppet module provisioning script

--- scripts/provision-puppet-modules.sh
+++ scripts/provision-puppet-modules.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-
 # Install the required modules that we need to provision the rabbitmq and ldap server
 
-# puppet module install puppetlabs-stdlib
-# puppet module install puppetlabs-apt
-# puppet module install puppet-rabbitmq
-# puppet module install puppet-archive
-# puppet module install camptocamp-openldap
-# puppet module install garethr-erlang
+puppetModules=(puppetlabs-stdlib puppetlabs-apt puppet-rabbitmq camptocamp-openldap)
 
-puppetModules=('puppetlabs-stdlib' 'puppetlabs-apt' 'puppet-rabbitmq' 'camptocamp-openldap' 'garethr-erlang')
-
-for m in $puppetModules; do
+for m in ${puppetModules[@]}; do
     puppet module install $m
 done
